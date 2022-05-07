4b38a976030bfd1691d20c1d537de567ae3a70b7
MariaK1506 <93434705+MariaK1506@users.noreply.github.com>
2022-05-07 10:24:07 +0300

    commit

--- js/task-01.js
+++ js/task-01.js
@@ -1,34 +1,26 @@
 // 1--
 const quantityItemEl = document.querySelectorAll(".item");
-console.log('Number of categories: ', quantityItemEl.length);
+console.log(`Number of categories: ${quantityItemEl.length}`);
 
 // 2--
 
+// 1 вариант
 const category = [...quantityItemEl]
-    .map(element =>`Category: ${element.children[0].textContent}
-    Elements: ${element.children[1].children.length}`)
-   .join("\n");
+  .map((element) => {
+    const firstEl = element.firstElementChild.textContent;
+    const secondEl = element.children[1].children.length;
+    return `Category: ${firstEl}\nElements: ${secondEl}`;
+  })
+  .join("\n");
 
 console.log(category);
 
-// const textItemEl = document.querySelectorAll("h2");
-// // textItemEl.forEach(element => {
-// //     console.log('Category: ', element.textContent)
-//     
-// ;
+// 2 вариант
+// const category = [...quantityItemEl]
+//   .map(
+//     (element) =>
+//       `Category: ${element.firstElementChild.textContent}\nElements: ${element.children[1].children.length}`
+//   )
+//   .join("\n");
 
-
-// const hhh = document.querySelectorAll(".item ul")
-
-// const length = hhh.forEach(element => {
-    
-//     console.log('Elements: ', element.querySelectorAll('li').length)
-// });
-    
-
-
-
-
-
-
-
+// console.log(category);
