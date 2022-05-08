750a30fb090e5d5d79db43d1764d56e7ba2546c4
MariaK1506 <93434705+MariaK1506@users.noreply.github.com>
2022-05-08 17:10:22 +0300

    commit

--- js/task-07.js
+++ js/task-07.js
@@ -6,6 +6,8 @@ const refs = {
 refs.input.addEventListener("input", onInputChange);
 
 function onInputChange() {
+  //   console.log(refs.input.value);
   const sizeText = refs.input.value + "px";
+
   refs.span.style.fontSize = sizeText;
 }
